7a6925a925ed4eb02f4e231e5fad6efed9e32e93
Karl Hallsby <karl@hallsby.com>
2022-07-17 12:52:04 -0500

    Add additional packages to manifest.scm, mostly for man-pages

--- manifest.scm
+++ manifest.scm
@@ -1,5 +1,9 @@
 (use-modules (gnu packages base)
-             (gnu packages commencement))
+             (gnu packages commencement)
+             (gnu packages gdb)
+             (gnu packages man))
 
 (packages->manifest
-  (list gcc-toolchain gnu-make))
+ (list gcc-toolchain gnu-make
+       gdb
+       man-db man-pages))
